c4cd13551d80fe4f6a1a49b47a58731b2ee785a2
rr2x <79620740+rr2x@users.noreply.github.com>
2022-03-15 19:22:39 +0800

    accordion, alerts, toasts, modals

---
+++ 34-toasts.js
@@ -0,0 +1,11 @@
+document.querySelector("#basicToastBtn").onclick = function() {
+ new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+
+document.querySelector("#basicToastBtn_2").onclick = function() {
+  new bootstrap.Toast(document.querySelector('#basicToast_2')).show();
+ }
+
+ document.querySelector("#infoToastBtn_2").onclick = function() {
+  new bootstrap.Toast(document.querySelector('#infoToast_2')).show();
+ }
